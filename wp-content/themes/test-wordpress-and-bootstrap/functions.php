3b170ea56d2569e854bb943f895dd943b8be8515
George Tourtsinakis <tourtsinakis@outlook.com>
2016-11-16 14:32:01 +0200

    Styiling posts

--- wp-content/themes/test-wordpress-and-bootstrap/functions.php
+++ wp-content/themes/test-wordpress-and-bootstrap/functions.php
@@ -149,4 +149,12 @@ add_filter( 'post_thumbnail_html', 'remove_image_size_attributes' );
 
 // Remove image size attributes from images added to a WordPress post
 add_filter( 'image_send_to_editor', 'remove_image_size_attributes' );
- 
+
+//Replace the excerpt "more" text by a link
+function new_excerpt_more($more){
+	global $post;
+	return '... <a class="moretag" href="'. get_permalink($post->ID) . '"> continue reading &raquo;</a>';
+
+}
+//It adds the new function to the orginal wordpress function
+add_filter('excerpt_more','new_excerpt_more');
